71d41bd81f81e6a9ced0bc3f5487732c8e5027b1
LeoHL <leonidgli57@gmail.com>
2024-05-11 17:15:42 +0200

    fix camera script

--- src/camera_follow.gd
+++ src/camera_follow.gd
@@ -6,7 +6,7 @@ func _process(delta):
 #this makes sure that the camera cannot go out of bounds the map, assuming the map starts at 0,0.
 #however, this does not chenck for lower and right bounds only top and left. also, only works in 32x32
 	self.global_position = Player.global_position
-	if self.global_position.x < 83:
-		self.global_position.x = 83
-	if self.global_position.y < 33:
-		self.global_position.y = 33
+	if self.global_position.x < 160:
+		self.global_position.x = 160
+	if self.global_position.y < 76:
+		self.global_position.y = 76
