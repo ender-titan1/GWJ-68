88fe5b53756feff4d0e2102b480475c2812d21cb
ender-titan1 <supszym02@gmail.com>
2024-05-11 14:31:55 +0200

    Get player and wall collisions to work
    Also simplified tiles into one tileset

--- src/Player.gd
+++ src/Player.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 @export var speed: float = 50.0
 
-func _process(delta: float) -> void:
+func _physics_process(delta):
 	var x_axis = Input.get_axis("ui_left", "ui_right")
 	var y_axis = Input.get_axis("ui_up", "ui_down")
-	var move := Vector2(x_axis,y_axis)
+	var dir := Vector2(x_axis, y_axis)
 	
-	#movement
-	position += move * speed * delta
+	# set velocity
+	velocity = dir * speed
+	
+	# call move_and_slide to collide with the walls
+	move_and_slide()
