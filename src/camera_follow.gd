ca4be20dad549e85f4a0b2d30be6706f4af0ec30
LeoHL <leonidgli57@gmail.com>
2024-05-11 16:11:46 +0200

    change textures, finish camera script

--- src/camera_follow.gd
+++ src/camera_follow.gd
@@ -7,8 +7,10 @@ func _ready():
 
 
 func _process(delta):
+#this makes sure that the camera cannot go out of bounds the map, assuming the map starts at 0,0.
+#however, this does not chenck for lower and right bounds only top and left. also, only works in 32x32
 	self.global_position = Player.global_position
-	if self.global_position.x < 0:
-		self.global_position.x = 0
-	if self.global_position.y < 0:
-		self.global_position.y = 0
+	if self.global_position.x < 83:
+		self.global_position.x = 83
+	if self.global_position.y < 33:
+		self.global_position.y = 33
