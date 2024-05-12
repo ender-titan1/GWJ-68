e37c00e515a3c534355b2862e4967e93f99adbf4
ender-titan1 <supszym02@gmail.com>
2024-05-12 14:03:57 +0200

    Add vision cone control to patrol.gd

--- src/patrol.gd
+++ src/patrol.gd
@@ -1,17 +1,71 @@
 extends CharacterBody2D
 
+# Exports
 @export var player: CharacterBody2D
 @export var vision_range: float
 @export var max_degrees: float
+@export var vision_cone_multiplier: float
 
+# Nodes
+@onready var vision_cone: Sprite2D = $"Sprite2D2"
+
+# Variables
 var max_radians: float
+var forward_vector: Vector2
 var forward_radians: float
 
 func _ready():
 	max_radians = deg_to_rad(max_degrees)
+	forward_radians = deg_to_rad(rotation_degrees)
+	forward_vector = Vector2(cos(forward_radians), sin(forward_radians))
+	
+	# Set up shader properties
+	vision_cone.material.set_shader_parameter("range", vision_range)
+	vision_cone.material.set_shader_parameter("rads", max_radians)
+	vision_cone.material.set_shader_parameter("fwd_vec", forward_vector)
+	vision_cone.material.set_shader_parameter("color", Color.AQUA)
+	
+	generate_texture()
+	
+func generate_texture():
+	var texture = GradientTexture2D.new()
+	texture.width = vision_range * 2 * vision_cone_multiplier
+	texture.height = vision_range * 2 * vision_cone_multiplier
+	
+	texture.fill = GradientTexture2D.FILL_RADIAL
+	texture.fill_from = Vector2(0.5, 0.5)
+	texture.fill_to = Vector2(1.0, 0.0)
+	
+	var gradient = Gradient.new()
+	var gradient_data = {
+		0.0: Color.WHITE,
+		0.8: Color.TRANSPARENT
+	}
+	
+	gradient.offsets = gradient_data.keys()
+	gradient.colors = gradient_data.values()
+	
+	texture.gradient = gradient
+	
+	vision_cone.texture = texture
 
 func _process(delta):
-	queue_redraw()
+	var spotted = player_spotted()
+	
+	if spotted:
+		vision_cone.material.set_shader_parameter("color", Color.RED)
+	else:
+		vision_cone.material.set_shader_parameter("color", Color.AQUA)
+	
+func _physics_process(delta):
+	
+	# Update the shader's position 
+	vision_cone.material.set_shader_parameter("pos", position)
+	
+	# Update the forward vector and corresponding shader param.
+	forward_radians = deg_to_rad(rotation_degrees)
+	forward_vector = Vector2(cos(forward_radians), sin(forward_radians))
+	vision_cone.material.set_shader_parameter("fwd_vec", forward_vector)
 	
 func player_spotted() -> bool:
 	var player_pos = player.position
@@ -21,23 +75,12 @@ func player_spotted() -> bool:
 		return false
 	
 	# using the formula for degree between two vectors:
-	# cos(a) = u * v / |u| * |v|
+	# cos(a) = (u . v) / (|u| * |v|)
 	
 	var vector_to_player = (player_pos - position).normalized()
-	var forward_vector = Vector2(cos(forward_radians), sin(forward_radians))
 	var dot = vector_to_player.dot(forward_vector)
 	
 	# Since both vector are normalized, no need to calculate magnitude
 	var angle = acos(dot / 1)
-	draw_line(Vector2.ZERO, forward_vector * 50, Color.YELLOW)
 	
-	if angle > max_radians:
-		draw_line(Vector2.ZERO, player_pos - position, Color.RED)
-	else:
-		draw_line(Vector2.ZERO, player_pos - position, Color.GREEN)
-	
-	return true
-
-func _draw():
-	player_spotted()
-	
+	return angle <= max_radians
