3a089a37ae7195f01774f0b63c2c6e9ebae75cee
ender-titan1 <supszym02@gmail.com>
2024-05-15 10:46:47 +0200

    Update guard and finish dash

--- src/Player.gd
+++ src/Player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 @export var speed: float = 50.0
+@export var dash_range: float = 150.0
+
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var in_shadow: bool
 var dash_input: bool = false
 var can_dash: bool = true
 
@@ -33,14 +36,26 @@ func _physics_process(delta):
 		sprite.play("idle")
 
 func _process(_delta):
-	if dash_input:
-		var in_shadow = Util.pos_in_shadow(self, global_position)
+	in_shadow = Util.pos_in_shadow(self, global_position)
 
+	if dash_input:
+		queue_redraw()
+		update_can_dash()
+
+
+func update_can_dash():
+	if dash_input:
 		if !in_shadow:
 			can_dash = false
 			return
 
 		var target = get_global_mouse_position()
+		var distance_to_target = target.distance_to(global_position)
+
+		if distance_to_target > dash_range:
+			can_dash = false
+			return
+
 		var result = Util.raycast(self, global_position, target, Constants.RAYCAST_MASK)
 
 		# We want to check if nothing in in the way or at the end of the dash
@@ -48,19 +63,33 @@ func _process(_delta):
 			can_dash = false
 			return
 
+		var target_in_shadow = Util.pos_in_shadow(self, target)
+
+		if !target_in_shadow:
+			can_dash = false
+			return
+
 		can_dash = true
 
-
+func dash():
+	if can_dash:
+		global_position = get_global_mouse_position()
 
 func _input(event):
 	if event.is_action_pressed("player_move_special"):
 		dash_input = true
-		queue_redraw()
 	
 	if event.is_action_released("player_move_special"):
 		dash_input = false
+		dash()
 		queue_redraw()
 
 func _draw():
 	if dash_input:
-		draw_arc(Vector2.ZERO, 100, 0, TAU, 180, Color.GREEN)
+		draw_arc(Vector2.ZERO, dash_range, 0, TAU, 180, Color.GREEN)
+		var target = get_global_mouse_position()
+
+		var color = Color.GREEN if can_dash else Color.RED
+
+		draw_arc(target - global_position, 10, 0, TAU, 180, color)
+		draw_line(Vector2.ZERO, target - global_position, color)
--- src/patrol.gd
+++ src/patrol.gd
@@ -5,20 +5,36 @@ extends CharacterBody2D
 @export var vision_range: float
 @export var max_degrees: float
 @export var vision_cone_multiplier: float
+@export var speed: float
 
 # Nodes
 @onready var vision_cone: Sprite2D = $"Sprite2D2"
+@onready var path: Path2D = $".."
 
 # Variables
 var max_radians: float
 var forward_vector: Vector2
 var forward_radians: float
+var points: PackedVector2Array
+var next_point: Vector2
+var next_point_idx: int
+var initial_point: Vector2
 
 func _ready():
 	max_radians = deg_to_rad(max_degrees)
 	forward_radians = deg_to_rad(rotation_degrees)
 	forward_vector = Vector2(cos(forward_radians), sin(forward_radians))
 	
+	vision_cone.visible = true
+
+	# Set up the points to follow
+	for point_idx in path.curve.point_count:
+		points.append(path.curve.get_point_position(point_idx))
+
+	next_point_idx = 0
+	next_point = points[next_point_idx]
+	initial_point = next_point
+
 	# Set up shader properties
 	vision_cone.material.set_shader_parameter("range", vision_range)
 	vision_cone.material.set_shader_parameter("rads", max_radians)
@@ -26,7 +42,41 @@ func _ready():
 	vision_cone.material.set_shader_parameter("color", Color.AQUA)
 	
 	generate_texture()
+
+func _process(_delta):
+	var spotted = player_in_cone() && line_of_sight(player)
 	
+	if spotted:
+		vision_cone.material.set_shader_parameter("color", Color.RED)
+	else:
+		vision_cone.material.set_shader_parameter("color", Color.AQUA)
+	
+func _physics_process(delta):
+	var dir = (next_point - global_position).normalized()
+	velocity = dir * speed * delta
+	
+	move_and_slide()
+
+	# Update the shader's position 
+	vision_cone.material.set_shader_parameter("pos", global_position)
+	
+	# Update the forward vector and corresponding shader param.
+	forward_radians = deg_to_rad(rotation_degrees)
+	forward_vector = dir
+	vision_cone.material.set_shader_parameter("fwd_vec", forward_vector)
+
+	# Check which point to go to next
+	var dist = global_position.distance_to(next_point)
+
+	if dist < 0.1:
+		next_point_idx += 1
+		next_point = points[next_point_idx]
+
+		if next_point == initial_point:
+			next_point = initial_point
+			next_point_idx = 0
+
+
 func generate_texture():
 	var texture = GradientTexture2D.new()
 	texture.width = vision_range * 2 * vision_cone_multiplier
@@ -49,22 +99,8 @@ func generate_texture():
 	
 	vision_cone.texture = texture
 
-func _process(_delta):
-	var spotted = player_in_cone() && line_of_sight(player)
-	
-	if spotted:
-		vision_cone.material.set_shader_parameter("color", Color.RED)
-	else:
-		vision_cone.material.set_shader_parameter("color", Color.AQUA)
-	
-func _physics_process(_delta):
-	# Update the shader's position 
-	vision_cone.material.set_shader_parameter("pos", position)
-	
-	# Update the forward vector and corresponding shader param.
-	forward_radians = deg_to_rad(rotation_degrees)
-	forward_vector = Vector2(cos(forward_radians), sin(forward_radians))
-	vision_cone.material.set_shader_parameter("fwd_vec", forward_vector)
+	#vision_cone.material = ShaderMaterial.new()
+	#vision_cone.material.shader = load("res://src/shader/light_cone.gdshader")
 	
 func player_in_cone() -> bool:
 	return Util.in_vision_cone(self, player, forward_vector, max_radians, vision_range)
